558651be404eaf3543605f9434614820f20bf1da
10ne2 <rla9513@naver.com>
2026-04-23 20:40:33 +0900

    ✨메인 배너 완성

--- src/components/MainAd.jsx
+++ src/components/MainAd.jsx
@@ -12,36 +12,55 @@ const mainAdImage = [ mainAdImg1, mainAdImg2,mainAdImg3, mainAdImg4]
 
 const MainAd = () => {
 
-  const [index, setindex] = useState(0);
+  const [index, setindex] = useState(1);
+  const [transition, setTransition] = useState(true); // 트랜지션 방법 : 기본 - 애니메이션 있도록
+
+  const extendedImages = [
+    mainAdImage[mainAdImage.length - 1], // 이미지 배열 마지막
+    ...mainAdImage, // 이미지 배열들 1, 2, 3, 4
+    mainAdImage[0], // 이미지 배열 첫번째
+  ];
 
   useEffect (() => {
     const interval = setInterval (() => {
-      setindex((prev) => (prev+1) % mainAdImage.length);
-    }, 10000);
+      setTransition(true);
+      setindex((prev) => (prev+1));
+    }, 3000);
 
     return () => clearInterval(interval);
   }, [])
 
-  const getPosition = (key) => {
-    if (key === index) return "center";
-    if (key === (index - 1 + mainAdImage.length) % mainAdImage.length) return "left";
-    if (key === (index +1) % mainAdImage.length) return "right";
-    return "hidden";
+  const transitionEnd = () => {
+    if (index === extendedImages.length - 1) {
+      setTransition(false);
+      setindex(1);
+    }
+    if (index === 0) {
+      setTransition(false);
+      setindex(mainAdImage.length);
+    }
   }
 
   const nextSlide = () => {
-    setindex((prev) => (prev + 1) % mainAdImage.length);
+    setTransition(true);
+    setindex((prev) => (prev + 1));
   };
   const prevSlide = () => {
-    setindex((prev) => (prev - 1 + mainAdImage.length) % mainAdImage.length);
+    setTransition(true);
+    setindex((prev) => (prev - 1));
   };
 
   return (
-    <banner id="mainAd">
+    <div id="mainAd">
       <div className="mainAd__inner">
-        <div className="mainAd__track">
-          {mainAdImage.map((img, key) => (
-            <div className={`slideItem ${getPosition(key)}`} key={key}>
+        <div  className="mainAd__track"
+              onTransitionEnd={transitionEnd}
+              style={{
+                transform: `translateX(-${index * 100}%)`,
+                transition: transition ? "0.5s ease" : "none",
+              }}>
+          {extendedImages.map((img, key) => (
+            <div className="slideItem" key={key}>
                 <img src={img} alt={`배너 이미지${key}`}/>
             </div>
         ))}
@@ -50,13 +69,15 @@ const MainAd = () => {
           <button className='arrowButton' onClick={prevSlide}>
             <img className='left' src={mainAdLeft} alt="왼쪽 화살표"/>
           </button>
-            <span>{index+1} / {mainAdImage.length}</span>
+            <span>
+              {((index - 1 + mainAdImage.length) % mainAdImage.length) + 1} / {mainAdImage.length}
+            </span>
           <button className='arrowButton' onClick={nextSlide}>
             <img className='right' src={mainAdRight} alt="오른쪽 화살표"/>
           </button>
         </div>
       </div>
-    </banner>
+    </div>
   )
 }
 
